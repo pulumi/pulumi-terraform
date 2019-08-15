5950e1b67891d4dc2193b15bb31bab757d0dcb78
Pat Gavlin <pat@pulumi.com>
2019-08-15 13:54:37 -0700

    Appease the linter

--- pkg/tfbridge/diff_test.go
+++ pkg/tfbridge/diff_test.go
@@ -33,8 +33,7 @@ func diffTest(t *testing.T, sch map[string]*schema.Schema, info map[string]*Sche
 	res := &schema.Resource{
 		Schema: sch,
 		CustomizeDiff: func(d *schema.ResourceDiff, _ interface{}) error {
-			d.SetNewComputed("outp")
-			return nil
+			return d.SetNewComputed("outp")
 		},
 	}
 	provider := &schema.Provider{
@@ -47,7 +46,7 @@ func diffTest(t *testing.T, sch map[string]*schema.Schema, info map[string]*Sche
 	attrs, meta, err := MakeTerraformAttributes(res, stateMap, sch, info, resource.PropertyMap{}, false)
 	assert.NoError(t, err)
 
-	config, err := MakeTerraformConfig(nil, inputsMap, sch, info, resource.PropertyMap{}, false)
+	config, err := MakeTerraformConfig(nil, inputsMap, sch, info, nil, resource.PropertyMap{}, false)
 	assert.NoError(t, err)
 
 	tfDiff, err := provider.SimpleDiff(&terraform.InstanceInfo{Type: "resource"},
