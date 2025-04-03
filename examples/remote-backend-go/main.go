13ab79ec302a4104f37153f3c42d8e9bf5e920dd
Ling Yang <ling@pulumi.com>
2025-04-03 14:32:40 -0400

    update Go SDK

--- examples/remote-backend-go/main.go
+++ examples/remote-backend-go/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 
-	"github.com/pulumi/pulumi-terraform/sdk/v5/go/state"
+	"github.com/pulumi/pulumi-terraform/sdk/go/state/provider"
 )
 
 func main() {
@@ -15,18 +15,22 @@ func main() {
 		organization := conf.Require("organization")
 		workspace := conf.Require("workspaceName")
 
-		state, err := state.NewRemoteStateReference(ctx, "remote-backend-state", &state.RemoteBackendStateArgs{
-			Organization: pulumi.String(organization),
-			Token:        pulumi.StringPtrInput(token.(pulumi.StringOutput)),
-			Workspaces: state.WorkspaceStateArgs{
-				Name: pulumi.String(workspace),
+		state, err := provider.RemoteStateReference(ctx, &provider.RemoteStateReferenceArgs{
+			// TODO: This should be optional as it's hardcoded somewhere by default
+			BackendType: "remote",
+			BackendConfig: provider.BackendConfig{
+				Organization: organization,
+				Token:        token.ElementType().String(),
+			},
+			Workspaces: provider.WorkspaceStateArgs{
+				Name: &workspace,
 			},
 		})
 		if err != nil {
 			return err
 		}
 
-		ctx.Export("test", state.Outputs)
+		ctx.Export("test", state.Outputs())
 
 		return nil
 	})
